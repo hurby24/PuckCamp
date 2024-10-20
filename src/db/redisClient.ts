2f6abb6e9871cf4f69cacc49796796667ea74bc5
Hebib Qurbanov <qurbanovhebib554@gmail.com>
2024-10-20 16:29:18 +0400

    small fix to redis

--- src/db/redisClient.ts
+++ src/db/redisClient.ts
@@ -1,22 +1,22 @@
 import { Redis } from "ioredis";
 
 export const getRedisClient = () => {
-	const redisUrl = process.env.REDIS_URL;
+  const redisUrl = process.env.REDIS_URL;
 
-	if (
-		process.env.NODE_ENV === "development" ||
-		process.env.NODE_ENV === "test"
-	) {
-		if (!redisUrl) {
-			throw new Error("REDIS_URL is not defined in development environment");
-		}
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "test"
+  ) {
+    if (!redisUrl) {
+      throw new Error("REDIS_URL is not defined in development environment");
+    }
 
-		return new Redis(redisUrl, {
-			tls: {
-				rejectUnauthorized: false,
-			},
-		});
-	}
+    return new Redis(redisUrl, {
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  }
 
-	return new Redis();
+  return new Redis("redis://puck-redis:6379");
 };
